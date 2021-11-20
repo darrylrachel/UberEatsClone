f017b6ef756083fd81fa5bffaa4011237345c4bc
Darryl Rachel <darryl@darryls-mbp.home>
2021-11-20 11:43:08 -0800

    initial commit

---
+++ components/HeaderTabs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HeaderTabs = (props) => {
+  return (
+    <View>
+      <HeaderButton />
+      {/* HeaderButton */}
+    </View>
+  );
+};
+
+
+const HeaderButton = () => <Text>Delivery</Text>;
+
+
+
+export default HeaderTabs;
